5314ecf3135505ca2d5f92fff63bad98f3cd29dc
Luis Carlos <roxisxprol@gmail.com>
2024-09-16 18:47:26 -0300

    fix: request Add Grupo

--- src/BoraRachar.Application/GruposCases/AdicionarGrupos/AdicionarGruposRequest.cs
+++ src/BoraRachar.Application/GruposCases/AdicionarGrupos/AdicionarGruposRequest.cs
@@ -1,11 +1,11 @@
 using BoraRachar.Domain.Service.Abstract.Dtos.Bases.Responses;
-using BoraRachar.Domain.Service.Abstract.Dtos.Categorias.AddCategoria;
+using BoraRachar.Domain.Service.Abstract.Dtos.Grupos.AddGrupo;
 using BoraRachar.Infra.CrossCuting;
 using MediatR;
 
 namespace BoraRachar.Application.GruposCases.AdicionarGrupos;
 
-public class AdicionarGrupoRequest : AddCategoriaRequestDto, IRequest<ResponseDto<None>>
+public class AdicionarGrupoRequest : AddGrupoRequestDto, IRequest<ResponseDto<None>>
 {
 
 }
